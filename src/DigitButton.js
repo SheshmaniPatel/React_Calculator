0115ad66836f4c17583c718b5a679f9303f51b55
Sheshmani Patel <sumitkumar47581@gmail.com>
2023-08-02 00:52:41 +0530

    Making calculator project using React

---
+++ src/DigitButton.js
@@ -0,0 +1,12 @@
+import { ACTIONS } from "./App"
+// making an export function for digit buttons
+
+export default function DigitButton({ dispatch, digit }) {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  )
+}
